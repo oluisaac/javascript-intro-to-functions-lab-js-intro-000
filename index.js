4fcb1891bffb16f383d97b9709c87a0a53e4963d
Olu Isaac <oluisaac7@gmail.com>
2019-03-14 11:31:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper (string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma (string){
+  console.logShout (" I love you, Grandma.")
+}
+
+function logShout (string){
+  console.logShout ()
+}
